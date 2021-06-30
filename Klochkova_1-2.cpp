3bb4e78085ed07f5ed767758b77d9d9c5e3a10e8
Stasy Black <klochkova.03@list.ru>
2021-06-30 16:50:34 +0300

    Day 1, practice, working with arrays

---
+++ Klochkova_1-2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <Windows.h> 
+#include <limits>
+using namespace std;
+
+int testrShiftMas();
+void rShiftMas(int* source, int n, int size);
+int source[1000];
+int size0 = 10;
+int testrShiftMas()
+{
+    int source[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int size = 10;
+    int n = 3;
+    if (n <= size)
+        rShiftMas(source, n, size);
+    else
+        return -1;
+    return 0;
+}
+void rShiftMas(int* source, int n, int size)
+{
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = size-1; i >= j; i--)
+        {
+            int m;
+            m = source[i - 1];
+            source[i] = m;
+            if (i == 0)
+                source[i] = 0;
+        }
+    }
+    for (int i = 0; i < size0; i++)
+        cout << source[i];
+    cout << "\n";
+}
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n1, i;
+    if (testrShiftMas() == -1)
+        cout << "Неправильные исходные данные";
+    else
+        cout << "Тест ок";
+}
