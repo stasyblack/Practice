4c80af26fe47cfba8702c330147ec0fff715be18
Stasy Black <klochkova.03@list.ru>
2021-07-03 14:35:13 +0300

    task 3-4, shift elements in list

--- listutils.cpp
+++ listutils.cpp
@@ -1,99 +1,144 @@
 #include <iostream>
+#include <Windows.h>
+using namespace std;
+
+const int m = 5;// сдвиг списка на m элементов
+const int n = 10;//количество элементов в списке
 
 struct Node {
   int val;
+  void init(Node& node);
   Node* next;
 };
 
+class List
+{
+    Node* head;
+public:
+    List() :head(NULL) {}; //Конструктор по умолчанию (Head=NULL)
+    ~List();    //Прототип деструктора
+    void initNode(int n);
+    void show();
+    void pushNode(Node& node);
+    void rShiftList(Node& node, int m);
+    int testEmptyList();
+    int testrShiftList(int n, int m);
+};
+
+List::~List()
+{
+    while (head != NULL)                //Пока по адресу есть хоть что-то
+    {
+        Node* temp;
+        temp = head->next;     //Сразу запоминаем указатель на адрес следующего элемента структуры
+        delete head;                    //Освобождаем память по месту начала списка
+        head = temp;                    //Меняем адрес начала списка
+    }
+
+}
+
 // Инициализации элемента односвязного списка нулями
 
-void init(Node* node) {
-
+void Node::init(Node& node)
+{
+    node.val = 12;//начальное значение для добавляемого элемента в список
 }
 
 // Отображения односвязного списка на экран
-void show(Node* head) {
+void List::show() 
+{
+    Node* temp;
+    temp = head;// указатель на начало списка
+    while (temp != NULL) //пока не дошли до конца списка
+    {
+        //вывод элементa структуры
+        cout << temp->val << "\t";
+        temp = temp->next; // указываем на следующий адрес из списка
+    }
+    cout << endl;
+}
 
+void List::initNode(int n)
+{
+    Node* temp;
+    int i;
+    int initVal = 0;
+    for (i = 0; i < n; i++)
+    {
+        temp = new Node; //выделение памяти под новую структуру
+        temp->next = head;
+        temp->val = initVal;
+        head = temp;// смена адреса начала списка
+    }
 }
 
 // Добавления элемента в односвязный список
-void pushNode(Node* head,Node* node) {
-
+void List::pushNode(Node& node)
+{
+    Node* temp;
+    temp = new Node; //выделение памяти под новую структуру
+    temp->next = head;
+    temp->val = node.val;
+    head = temp;// смена адреса начала списка
 } 
 
 
-// Написать функцию инициализации size элементов списка заданным значением val.
-void initList(Node* head, int size, int val)  {
-
-}
-
-// Написать функцию копирования элементов массива source в новый массив dest
-void copyMas(Node* source, Node* dest) {
-
-}
-  
-
-// Написать функцию сравнение списков source и dest. Если списки равны 0, если не равны -1
-int compareMas(Node* source, Node* dest) {
-  return 0;
-}
-
-
-// Написать функцию удаления со сдвигом влево n-го элемента списка.
-void deleteList(Node* head,int n) {
-
-}
-
-
 // Написать функцию сдвига значений вправо на n-элементов с заполнением крайних элементов нулями
-void rShiftList(Node* head,int n) {
-
-}
-
-
-
-// Написать функцию сдвига значений влево на n-элементов с заполнением крайних элементов нулями.
-void lShiftList(Node* head,int n) {
-
-}
-
-
-//Написать функцию сдвига значений влево на n-элементов с переносом вытесненных элементов в конец
-void lRoundShiftMas(Node* head,int n, int size) {
-
-}
-
-
-// Написать функцию вычисления среднего значения элементов списка.
-int averageList(Node* head, int size) {
-  return 0;
-}
-
-
-
-// Написать функцию вычисления средне квадратического отклонения элементов списка.
-int skoFromList(Node* head, int size) {
-  return 0;
-}
-
-
-int testSkoFromList() {
-  return -1;
-}
-
-
-static void runTest(int (*testFunction)(),const std::string& testName)
+void List::rShiftList(Node& node, int n)
 {
-  if(testFunction()==0)
-    std::cout << "Test "<< testName << " - OK" << std::endl;
-  else 
-    std::cout << "Test "<< testName << " - FAIL" << std::endl;
+    int i, num, numNext;
+    Node* temp;
+    for (i = 0; i < n; i++)
+    {
+        temp = head;// указатель на начало списка
+        num = temp->val;
+        temp->val = 0;
+        while (temp->next != NULL) //пока не дошли до конца списка
+        {
+
+            temp = temp->next; // указываем на следующий адрес из списка
+            swap(temp->val, num);
+        }
+    }
 }
 
 
+int List::testEmptyList()
+{
+    if (head == NULL)
+    {
+        cout << "Ошибка: список пуст";
+        return -1;
+    }
+    return 0;
+}
 
-int main() {
-  runTest(testSkoFromList,"testSkoFromList");
+int List::testrShiftList(int n, int m)
+{
+    if (m > n + 1)
+    {
+        cout << "Ошибка: сдвиг превышает количество элементов";
+        return -1;
+    }
+    return 0;
+}
 
 
+int main() 
+{
+  SetConsoleCP(1251);
+  SetConsoleOutputCP(1251);
+  Node node;
+  List lst;
+  lst.initNode(n);
+  if (lst.testEmptyList() == -1)
+      return -1;
+  lst.show();
+  node.init(node);
+  lst.pushNode(node);
+  lst.show();
+  if (lst.testrShiftList(n, m) == -1)
+      return -1;
+  lst.rShiftList(node, m);
+  lst.show();
 }
