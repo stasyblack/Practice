069ff679386644ca8c7770e3db54eee29a20adf5
Stasy Black <klochkova.03@list.ru>
2021-07-05 20:36:49 +0300

    task 8-9-11, working with images, making pnm-files, class

--- imgutils.cpp
+++ imgutils.cpp
@@ -4,35 +4,48 @@
 using namespace std;
 struct Image 
 {
-  int mx;
-  int my;
+  int mx=6;
+  int my=10;
   int* points;  //картинка размером mx * my
+  void createImage(Image* img);
+  void showImage(Image* img);
+  void setPointImage(Image* img, int x, int y, int color);
+  int getPointImage(Image* img, int x, int y);
+  int testSaveToPnmFile(string fileName);
+  int saveToPnmFile(Image* img, string fileName);
+  int testCreateImage();
+  int testGetPointImage(int x, int y);
+  int testSetPointImage(int color);
+  void dataImage(Image* img);
 };
 
+void Image::dataImage(Image* img)
+{
+	points[4] = 1;
+	points[10] = 1;
+	points[16] = 1;
+	points[22] = 1;
+	points[28] = 1;
+	points[34] = 1;
+	points[40] = 1;
+	points[45] = 1;
+	points[44] = 1;
+	points[43] = 1;
+	points[36] = 1;
+}
 
 // Создание изображения
-void createImage(Image* img,int tmx, int tmy) 
+void Image::createImage(Image* img) 
 {
   // Выделить память  размером tmx * tmy 
-	img->mx = tmx;
-	img->my = tmy;
-	int n = tmx * tmy;
-	img->points = new int[n];
+	int n = mx * my;
+	points = new int[n];
 	for (int i = 0; i < n; i++)
-		img->points[i] = 0;
-	img->points[4] = 1;
-	img->points[10] = 1;
-	img->points[16] = 1;
-	img->points[22] = 1;
-	img->points[28] = 1;
-	img->points[34] = 1;
-	img->points[40] = 1;
-	img->points[45] = 1;
-	img->points[44] = 1;
-	img->points[43] = 1;
-	img->points[36] = 1;
+		points[i] = 0;
+	dataImage(img);
 
 }
+
 
 
 // Отображения изображения на экран в pnm формате
@@ -48,16 +61,16 @@ void createImage(Image* img,int tmx, int tmy)
 //  0 0 0 0 0 0
 
 
-void showImage(Image* img, int tmx, int tmy)
+void Image::showImage(Image* img)
 {
   // Вывести картинку на экран при помощи cout или 
 	int n;
-	for (int i = 0; i < tmy; i++)
+	for (int i = 0; i < my; i++)
 	{
-		for (int j = 0; j < tmx; j++)
+		for (int j = 0; j < mx; j++)
 		{
-			n = (i * tmx + j);
-			cout << img->points[n] << " ";
+			n = (i * mx + j);
+			cout << points[n] << " ";
 		}
 		cout << endl;
 	}
@@ -70,22 +83,21 @@ void showImage(Image* img, int tmx, int tmy)
 //  0 0 0 0 1 0  0 0 0 0 1 0  0 0 0 0 1 0  0 0 0 0 1 0  0 0 0 0 1 0  0 0 0 0 1 0  1 0 0 0 1 0  0 1 1 1 0 0  0 0 0 0 0 0  0 0 0 0 0 0
    
 
-void setPointImage(Image* img,int x, int y,int color,int tmx) 
+void Image::setPointImage(Image* img,int x, int y,int color) 
 {
 	int n;
-	n = x * tmx + y;
-	img->points[n] = color;
+	n = x * mx + y;
+	points[n] = color;
 }
 
 
 // Получение точки на изображение
-int getPointImage(Image* img,int x, int y, int tmx)
+int Image::getPointImage(Image* img,int x, int y)
 {
 	int n, color;
-	n = x * tmx + y;
-	color=img->points[n];
-	cout << "Точка (" << x << ", " << y << ") имеет цвет - " << color << endl;
-	return 0;
+	n = x * mx + y;
+	color=points[n];
+	return color;
 }
 
 
@@ -107,7 +119,7 @@ int getPointImage(Image* img,int x, int y, int tmx)
 //  Файл можно открыть в gimp
 
 
-int testSaveToPnmFile(string fileName)
+int Image::testSaveToPnmFile(string fileName)
 {
 	if (fileName == "")
 	{
@@ -117,20 +129,20 @@ int testSaveToPnmFile(string fileName)
 	return 0;
 }
 
-int saveToPnmFile(Image* img, string fileName, int tmx,int tmy) 
+int Image::saveToPnmFile(Image* img, string fileName) 
 {
 	if (testSaveToPnmFile(fileName) == -1)
 		return -1;
 	fstream fout(fileName, ios::out);
 	int n;
 	fout << "P1" << endl;
-	fout << tmx << " " << tmy << endl;
-	for (int i = 0; i < tmy; i++)
+	fout << mx << " " << my << endl;
+	for (int i = 0; i < my; i++)
 	{
-		for (int j = 0; j < tmx; j++)
+		for (int j = 0; j <mx; j++)
 		{
-			n = (i * tmx + j);
-			fout << img->points[n] << " ";
+			n = (i * mx + j);
+			fout << points[n] << " ";
 		}
 		fout << endl;
 	}
@@ -140,9 +152,9 @@ int saveToPnmFile(Image* img, string fileName, int tmx,int tmy)
 
 
 
-int testCreateImage(int tmx, int tmy)
+int Image::testCreateImage()
 {
-	if ((tmx <= 0) || (tmy <= 0))
+	if ((mx <= 0) || (my <= 0))
 	{
 		cout << "Ошибка ввода размера изображения";
 		return -1;
@@ -150,16 +162,16 @@ int testCreateImage(int tmx, int tmy)
 	return 0;
 }
 
-int testGetPointImage(int x, int y, int tmx, int tmy)
+int Image::testGetPointImage(int x, int y)
 {
-	if ((x > tmx) || (y > tmy))
+	if ((x > mx) || (y > my))
 	{
 		cout << "Ошибка в задании координаты точки";
 		return -1;
 	}
 }
 
-int testSetPointImage(int color)
+int Image::testSetPointImage(int color)
 {
 	if ((color < 0) || (color > 255))
 	{
@@ -174,28 +186,27 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	Image img;
-	int tmx = 6;
-	int tmy = 10;
 	int x = 1;
 	int y = 4;
 	int color = 0;
-	if (testCreateImage(tmx, tmy) == -1)
+	if (img.testCreateImage() == -1)
 		return -1;
-	createImage(&img, tmx, tmy);
-	showImage(&img,tmx,tmy);
-	if (testGetPointImage(x, y, tmx, tmy) == -1)
+	img.createImage(&img);
+	img.showImage(&img);
+	if (img.testGetPointImage(x, y) == -1)
 		return -1;
-	if (testSetPointImage(color) == -1)
+	if (img.testSetPointImage(color) == -1)
 		return -1;
-	setPointImage(&img, x, y, color,tmx);
-	showImage(&img, tmx, tmy);
-	if (saveToPnmFile(&img, "image1.pnm", tmx, tmy) == -1)
+	img.setPointImage(&img, x, y, color);
+	img.showImage(&img);
+	if (img.saveToPnmFile(&img, "image1.pnm") == -1)
 		return -1;
 	x = 2;
 	y = 4;
-	if (testGetPointImage(x, y, tmx, tmy) == -1)
+	if (img.testGetPointImage(x, y) == -1)
 		return -1;
-	getPointImage(&img, x,  y, tmx);
+	cout<< "Цвет точки с координатами ("<<x<<", "<<y<<"):" << img.getPointImage(&img, x,  y);
+
 	delete[] img.points;
 	
 	return 0;
