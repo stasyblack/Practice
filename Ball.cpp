22b10dde92068c9f4ba96488f22d0e05fad372ad
Stasy Black <klochkova.03@list.ru>
2021-07-06 17:50:44 +0300

    task 6-7-10, var 2, flight of ball, image

--- Ball.cpp
+++ Ball.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <fstream> 
 #include <Windows.h> 
 #include <limits>
 
@@ -10,11 +11,17 @@ const double g = 10;
 
 struct flight
 {
+    int mh = 120;
+    int mt = 57;
+    int* points;
     double v=0;
     double a=0;
     double h=0;
     double t=0;
     double InputFlight(flight n_flight, double v0, double h0, double t0);
+    void createImage(int t, int h);
+    void initImage();
+    void saveToPnmFile(string fileName);
     flight* next;
 };
 
@@ -105,6 +112,42 @@ void List::ShowFlight(double t1,double t2)
     cout << endl;
 }
 
+void flight::initImage()
+{
+    // Выделить память  размером tmx * tmy 
+    int n = mt * mh;
+    points = new int[n];
+    for (int i = 0; i < n; i++)
+        points[i] = 0;
+}
+
+void flight::createImage(int t, int h)
+{
+    int n;
+    n = h * mt + t;
+    points[n] = 1;
+
+}
+
+void flight::saveToPnmFile( string fileName)
+{
+    fstream fout(fileName, ios::out);
+    int n;
+    fout << "P1" << endl;
+    fout << mt << " " << mh << endl;
+    for (int i = mh-1; i >=0; i--)
+    {
+        for (int j = 0; j < mt; j++)
+        {
+            n = (i * mt + j);
+            fout << points[n] << " ";
+        }
+        fout << endl;
+    }
+    fout << endl;
+    fout.close();
+}
+
 int main()
 {
     SetConsoleCP(1251);
@@ -116,7 +159,8 @@ int main()
     double m = 1;//масса шарика
     double h0 = 100;//начальная высота
     double t0;
-    double t1 = 0.5;//диапазон времени полета шарика
+    double h1=100;
+    double t1 = 0;//диапазон времени полета шарика
     double t2 = 3;
     double dt = 0.1;//шаг дискретизации времени
     t0 = t1;
@@ -124,10 +168,14 @@ int main()
     {
         return -1;
     }
-    while (n_flight.InputFlight(n_flight, v0, h0, t0) >=0)
+    n_flight.initImage();
+    while (h1 >=0)
     {
+        h1 = n_flight.InputFlight(n_flight, v0, h0, t0);
+        n_flight.createImage(t0*10, h1);
         lst.AddFlight(n_flight);
         t0 = t0 + dt;
     }
     lst.ShowFlight(t1,t2);
+    n_flight.saveToPnmFile("imageB.pnm");
 }
